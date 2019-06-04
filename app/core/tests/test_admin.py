ef4917ac28260ebcc31f60cc8b11c3e9bb6698aa
Estela Medrano <stlmedrano@gmail.com>
2019-06-04 12:53:12 +0200

    modify Django admin to list our costom user model

---
+++ app/core/tests/test_admin.py
@@ -0,0 +1,26 @@
+from django.test import TestCase, Client
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+
+class AdminSiteTests(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email='yo@gmia.com',
+            password='password123'
+        )
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            email='tes@gmail.com',
+            password='pass123',
+            name='Test unser full name'
+        )
+
+    def test_user_listed(self):
+        url = reverse('admin:core_user_changelist')
+        result = self.client.get(url)
+
+        self.assertContains(result, self.user.name)
+        self.assertContains(result, self.user.email)
