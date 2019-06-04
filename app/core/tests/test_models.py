7885595f18cbf8fe61c5ba1d2bce0ec11f66ca15
Estela Medrano <stlmedrano@gmail.com>
2019-06-04 12:14:26 +0200

    create superuser funcionality

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -19,13 +19,20 @@ class ModelTests(TestCase):
     def test_new_user_email_normalized(self):
         email = 'stlmedrano@GMAIL.com'
         test_password = 'test123'
-
         user = get_user_model().objects.create_user(email, test_password)
 
         self.assertEqual(user.email, email.lower())
 
     def test_new_user_invalid_email(self):
         test_password = 'test123'
-
         with self.assertRaises(ValueError):
             get_user_model().objects.create_user(None, test_password)
+
+    def test_create_new_superuser(self):
+        user = get_user_model().objects.create_superuser(
+            'sfs@gmail.com',
+            'test123'
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
