530988ef9aeaba19685bd5e81743bbee5166f376
Estela Medrano <stlmedrano@gmail.com>
2019-06-10 13:52:02 +0200

    add manage user endpoint

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -14,6 +14,16 @@ class UserSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         return get_user_model().objects.create_user(**validated_data)
 
+    def update(self, instance, validated_data):
+        password = validated_data.pop('password', None)
+        user = super().update(instance, validated_data)
+
+        if password:
+            user.set_password(password)
+            user.save()
+
+        return user
+
 
 class AuthTokenSerializer(serializers.Serializer):
 
@@ -24,7 +34,6 @@ class AuthTokenSerializer(serializers.Serializer):
     )
 
     def validate(self, attrs):
-
         email = attrs.get('email')
         password = attrs.get('password')
 
@@ -35,7 +44,7 @@ class AuthTokenSerializer(serializers.Serializer):
         )
         if not user:
             msg = _('Unable to authenticate with provided credentials')
-            raise serializers.ValidationError(msg, code='authentication')
+            raise serializers.ValidationError(msg, code='authorization')
 
         attrs['user'] = user
         return attrs
