578c1dde0f02ec73ae17a234dd0c7cb6d6f9313d
Estela Medrano <stlmedrano@gmail.com>
2019-06-13 07:56:52 +0200

    Recipe list

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -2,7 +2,7 @@ from rest_framework import viewsets, mixins
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
-from core.models import Tag, Ingredient
+from core.models import Tag, Ingredient, Recipe
 
 from recipe import serializers
 
@@ -30,3 +30,12 @@ class IngredientViewSet(BaseRecipeAttrViewSet):
 
     queryset = Ingredient.objects.all()
     serializer_class = serializers.IngredientSerializer
+
+
+class RecipeViewSet(BaseRecipeAttrViewSet):
+
+    queryset = Recipe.objects.all()
+    serializer_class = serializers.RecipeSerializer
+
+    def get_queryset(self):
+        return self.queryset.filter(user=self.request.user).order_by('-title')
